f2bd67bdae5cc525e469fad23b40c32a3a42195c
RompeTecla <javalinogames@gmail.com>
2023-06-11 23:58:29 +0200

    Creado: archivo verifyToken.js en base a jsonwebtoken para dar o quitar privilegios a usuarios segun su roleId.

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    const bearerToken = req.headers.authorization;
+
+    if (!bearerToken) {
+      return res.json({
+        succes: true,
+        message: "No tienes permisos.",
+      });
+    }
+
+    const token = bearerToken.split(" ")[1];
+
+    const decoded = jwt.verify(token, "alohomora");
+
+    req.userId = decoded.userId;
+
+    req.roleId = decoded.roleId;
+
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Token no válido.",
+      error: error,
+    });
+  }
+};
+
+module.exports = auth;
