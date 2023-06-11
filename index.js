9c25e91bae3f180b0df5fe59bac21374a34793aa
RompeTecla <javalinogames@gmail.com>
2023-06-11 22:31:36 +0200

    Creados: seeders para la tabla Roles.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+
+app.get('/probando', (req, res) => {
+    return res.send('Probando 1, 2, 3... ¡EUREKA!');
+});
+
 app.listen(PORT, () => {
     console.log ("Servidor levantado en el puerto " + PORT)
 }) 
