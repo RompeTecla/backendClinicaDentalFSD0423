c6200be142929f1cc99cdcab22b5491092144007
RompeTecla <javalinogames@gmail.com>
2023-07-04 04:04:12 +0200

    Creación: nuevo midelo y migración para dentistas.

---
+++ models/dentist.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Dentist extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Dentist.hasMany(models.Appointment, {
+        foreignKey: 'dentist_id'
+      });
+
+      Dentist.belongsTo(models.Speciality, {
+        foreignKey: 'speciality_id'
+      });
+    }
+  }
+  Dentist.init({
+    user_id : DataTypes.INTEGER,
+    speciality_id: DataTypes.INTEGER,
+    registration_number: DataTypes.STRING,
+    
+  }, {
+    sequelize,
+    modelName: 'Dentist',
+  });
+  return Dentist;
+};
