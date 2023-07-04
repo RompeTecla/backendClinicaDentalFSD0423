7742b49758436b2a3d73f96f64e3eef3487d6aeb
RompeTecla <javalinogames@gmail.com>
2023-07-04 04:06:41 +0200

    Creación: nuevo midelo y migración para historia del paciente.

---
+++ migrations/20230301105820-create-history.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      pacient_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Pacients",
+          key: "id"
+        }
+      },
+      date: {
+        type: Sequelize.STRING
+      },
+      observation: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Histories');
+  }
+};
