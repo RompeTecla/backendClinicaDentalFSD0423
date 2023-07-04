c26c0f731b1b1f32926080057b6705a56c074cc2
RompeTecla <javalinogames@gmail.com>
2023-07-04 04:10:54 +0200

    Creación: nuevo midelo y migración para la facturación de los servicios a los pacientes..

---
+++ migrations/20230225180627-create-payment.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      treatment_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Treatments",
+          key: "id"
+        }
+      },
+      note: {
+        type: Sequelize.TEXT
+      },
+      date: {
+        type: Sequelize.STRING
+      },
+      amount: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Payments');
+  }
+};
---
+++ models/payment.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Payment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Payment.belongsTo(models.Treatment, {
+        foreignKey: 'treatment_id'
+      });
+    }
+  }
+  Payment.init({
+    treatment_id : DataTypes.INTEGER,
+    note: DataTypes.TEXT,
+    date: DataTypes.STRING,
+    amount: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Payment',
+  });
+  return Payment;
+};
