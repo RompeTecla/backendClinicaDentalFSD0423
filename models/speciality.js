6df45fd81d7d1e3c999eff0dcee4633285c1700f
RompeTecla <javalinogames@gmail.com>
2023-07-04 04:03:12 +0200

    Creación: nuevo midelo y migración para especialidades.

---
+++ models/speciality.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Speciality extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Speciality.hasMany(models.Dentist, {
+        foreignKey: 'speciality_id'
+      });
+    }
+  }
+  Speciality.init({
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Speciality',
+  });
+  return Speciality;
+};
